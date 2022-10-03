bbbe17009b6c9c89dd04ea8d106f9c224ee6cd45
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-10-03 19:32:00 +0300

    Final commit

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,17 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function BotCollection() {
+function BotCollection({ bots, onClickBot, handleDelete }) {
   // Your code here
+  // console.log(addList)
+  const bot = bots.map((eachBot) => <BotCard key={eachBot.id} bot={eachBot} onClickBot={onClickBot} onDelete={handleDelete} />)
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
-        Collection of all bots
+        {bot}
+
+
       </div>
     </div>
   );
--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,54 @@
-import React from "react";
+
+import React, { useEffect, useState } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
   //start here with your code for step one
+  const [bots, setBots] = useState([])
+
+  const [items, setItems] = useState([])
+
+  function handleDelete(delItem) {
+
+    setItems(items.filter((item) => item.id !== delItem.id));
+
+    setBots(bots.filter((bot) => bot.id !== delItem.id));
+
+  }
+
+  function onClickBot(id) {
+
+    const found = items.find((item) => item.id === id);
+
+    if (!found) {
+
+      const i = bots.find((item) => item.id === id);
+      setItems(items => [...items, i]);
+    }
+    else {
+
+      const allItems = items.filter((item) => item.id !== found.id)
+      setItems(allItems);
+    }
+    //  console.log(items);
+
+  }
+  useEffect(() => {
+    fetch('http://localhost:8002/bots')
+      .then(res => res.json())
+      .then(data => setBots(data))
+  }, [])
+
+
+
+  // console.log(bots)
+
 
   return (
     <div>
-      <YourBotArmy />
-      <BotCollection />
+      <YourBotArmy bots={items} onClickBot={onClickBot} onDelete={handleDelete} />
+      <BotCollection bots={bots} onClickBot={onClickBot} onDelete={handleDelete} />
     </div>
   )
 }
