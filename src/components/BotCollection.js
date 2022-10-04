62b0c66016f8d9976ff1a00d84b2b0c7fdeb7c86
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-10-04 12:26:22 +0300

    Corrected BotCard file

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,10 +1,10 @@
 import React from "react";
 import BotCard from "./BotCard";
 
-function BotCollection({ bots, onClickBot, handleDelete }) {
+function BotCollection({ bots, onClickBot, onDelete }) {
   // Your code here
   // console.log(addList)
-  const bot = bots.map((eachBot) => <BotCard key={eachBot.id} bot={eachBot} onClickBot={onClickBot} onDelete={handleDelete} />)
+  const bot = bots.map((eachBot) => <BotCard key={eachBot.id} bot={eachBot} onClickBot={onClickBot} onDelete={onDelete} />)
   return (
     <div className="ui four column grid">
       <div className="row">
